f70ed3eaed99eadf47b96fecffa4c096ae407725
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2021-10-01 17:51:28 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,5 +1,3 @@
-#(©)Codexbotz
-
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
@@ -37,7 +35,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-  await second_message.reply_text(f"{link}", disable_web_page_preview = True)
+    await second_message.reply_text(f"{link}", disable_web_page_preview = True)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -57,4 +55,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-   await channel_message.reply_text(f"{link}", disable_web_page_preview = True, quote=False, reply_markup=reply_markup)
+    await channel_message.reply_text(f"{link}", disable_web_page_preview = True, quote=False, reply_markup=reply_markup)
